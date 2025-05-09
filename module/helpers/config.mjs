1c41a42fc2c98d4b30f3637c894ead7f8bf3b85e
ilya.vasiuk <ilya.vasiuk@gmail.com>
2025-05-09 09:35:09 +0200

    init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const CAVALRY = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+CAVALRY.abilities = {
+  str: 'CAVALRY.Ability.Str.long',
+  dex: 'CAVALRY.Ability.Dex.long',
+  con: 'CAVALRY.Ability.Con.long',
+  int: 'CAVALRY.Ability.Int.long',
+  wis: 'CAVALRY.Ability.Wis.long',
+  cha: 'CAVALRY.Ability.Cha.long',
+};
+
+CAVALRY.abilityAbbreviations = {
+  str: 'CAVALRY.Ability.Str.abbr',
+  dex: 'CAVALRY.Ability.Dex.abbr',
+  con: 'CAVALRY.Ability.Con.abbr',
+  int: 'CAVALRY.Ability.Int.abbr',
+  wis: 'CAVALRY.Ability.Wis.abbr',
+  cha: 'CAVALRY.Ability.Cha.abbr',
+};
